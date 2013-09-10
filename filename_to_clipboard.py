be4b4b8094928394fa88a74dc8820e5ed00e406c
Ivan Storck <ivanoats@gmail.com>
2013-09-09 22:40:22 -0700

    adds menu item to edit menu

--- filename_to_clipboard.py
+++ filename_to_clipboard.py
@@ -1,3 +1,6 @@
+# This command is already built-in to Sublime text, in the right-click menu for any open file
+# But I wanted to practice making a sublime text plugin.
+# This plugin adds the command, and the menu file adds the command to the menu.
 import sublime, sublime_plugin
 
 class Filename_to_clipboardCommand(sublime_plugin.TextCommand):
